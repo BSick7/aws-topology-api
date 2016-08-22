b0b0a0907defd60fd11dd1940b0d9ae47a499fb9
Brad Sickles <brad_sickles@mckinsey.com>
2016-08-22 11:14:18 -0400

    Added logging to stdout.

--- agent/agent.go
+++ agent/agent.go
@@ -3,11 +3,15 @@ package agent
 import (
 	"github.com/BSick7/aws-topology-api/agent/api"
 	"github.com/BSick7/aws-topology-api/types"
+	"github.com/gorilla/handlers"
 	"net/http"
+	"os"
+	"log"
 )
 
 func Run(c *types.AgentConfig, appVersion string) error {
 	s := api.NewApiServer(c, appVersion)
 	s.Register()
-	return http.ListenAndServe(c.Bind.String(), s.Router)
+	log.Printf("Starting server %s...", c.Bind.String())
+	return http.ListenAndServe(c.Bind.String(), handlers.LoggingHandler(os.Stdout, s.Router))
 }
